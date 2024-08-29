5e0f45e02055407759580a761c0bddedb0fa8268
davijr78 <davi.starepravo.jr@outlook.com>
2024-08-29 19:59:49 -0300

    commit

--- main.py
+++ main.py
@@ -1,7 +1,1 @@
-echo "# TestNN" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M maingo
-git remote add origin https://github.com/davijr78/TestNN.git
-git push -u origin main
+print("hello world")
